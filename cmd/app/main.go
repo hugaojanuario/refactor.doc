b628bb0f615fe9bf2cfab81d6a5018824832e1d0
Hugo Januário Santariosi - Siplan <hugo.santariosi@siplan.com.br>
2026-01-21 10:53:11 -0300

    main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"github.com/hugaojanuario/refactor.doc/internal/converter"
+	"log"
+)
+
+func main() {
+	fmt.Println("Iniciando conversao de arquivos...")
+
+	err := converter.ConvertTxtToDocx(
+		"input/exemplo.txt",
+		"output/exemplo.docx")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Conversão finalizada com sucesso!")
+}
