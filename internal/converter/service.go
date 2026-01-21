04624d1f530873229c010bf6d97536eb3d4765ac
Hugo Januário Santariosi - Siplan <hugo.santariosi@siplan.com.br>
2026-01-21 11:24:08 -0300

    Renewed RTF transcription

--- internal/converter/service.go
+++ internal/converter/service.go
@@ -1,1 +1,17 @@
 package converter
+
+func ConvertTxtToDocx(inputTxt string, outputDocx string) error {
+	rtfPath := "output/tempo.rtf"
+
+	err := TxtToRTF(inputTxt, rtfPath)
+	if err != nil {
+		return err
+	}
+
+	err = RTFToDocx(rtfPath, outputDocx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- internal/converter/txt_to_rtf.go
+++ internal/converter/txt_to_rtf.go
@@ -1,1 +1,19 @@
 package converter
+
+import (
+	"os"
+)
+
+func TxtToRTF(inputPath string, outputPath string) error {
+	content, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	// Estrutura básica de RTF
+	rtfContent := "{\\rtf1\\ansi\n"
+	rtfContent += string(content)
+	rtfContent += "\n}"
+
+	return os.WriteFile(outputPath, []byte(rtfContent), 0644)
+}
